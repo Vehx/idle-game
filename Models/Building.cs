e531923198b31e2b97b7c3e00be848fb299c7301
vehx <delta222@gmail.com>
2020-04-22 16:26:18 +0200

    added building contorller, model, dtos and service

---
+++ Models/Building.cs
@@ -0,0 +1,14 @@
+
+namespace Game.Models
+{
+    public class Building
+    {
+        public int Id { get; set;}
+        public string Name { get; set;} = "Farm";
+        public int Cost { get; set;} = 100;
+        public int IncomeIncrease { get; set;} = 1;
+        public double CostIncrease { get; set;} = 1.2;
+        public IdleUpgrades Upgrade { get; set;} = IdleUpgrades.DoubleIncome;
+    }
+
+}
