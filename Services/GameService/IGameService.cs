36a98139ef6fb04ac4439154af4bd7bafd0d3229
vehx <delta222@gmail.com>
2020-04-22 16:27:29 +0200

    added potential upgrade model and game service

---
+++ Services/GameService/IGameService.cs
@@ -0,0 +1,16 @@
+// using System.Collections.Generic;
+// using System.Threading.Tasks;
+// using Game.Models;
+// using Game.Dtos.Building;
+
+// namespace Game.Services
+// {
+//     public interface IGameService
+//     {
+//         Task<ServiceResponse<List<GetBuildingDto>>> GetAllBuildings();
+//         Task<ServiceResponse<GetBuildingDto>> GetBuildingById(int id);
+//         Task<ServiceResponse<List<GetBuildingDto>>> AddBuilding(AddBuildingDto newBuilding);
+//         Task<ServiceResponse<GetBuildingDto>> UpdateBuilding(UpdateBuildingDto updatedBuilding);
+//         Task<ServiceResponse<List<GetBuildingDto>>> DeleteBuilding(int id);
+//     } 
+// }
