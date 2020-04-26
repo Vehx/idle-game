8b686cae1449a8ce556047fde828fd8529f1bbaf
vehx <delta222@gmail.com>
2020-04-26 14:55:00 +0200

    added owned field to buildings

--- Models/Building.cs
+++ Models/Building.cs
@@ -5,6 +5,7 @@ namespace Game.Models
     {
         public int Id { get; set;}
         public string Name { get; set;} = "Farm";
+        public int Owned { get; set;} = 0;
         public int Cost { get; set;} = 100;
         public int IncomeIncrease { get; set;} = 1;
         public double CostIncrease { get; set;} = 1.2;
