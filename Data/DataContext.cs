37750456e695940f7e48d81c9b898c50e8a460f9
vehx <delta222@gmail.com>
2020-05-01 15:38:03 +0200

    added entity framework mysql configuration

--- Data/DataContext.cs
+++ Data/DataContext.cs
@@ -1,11 +1,16 @@
-// using Game.Models;
-// using Microsoft.EntityFrameworkCore;
+using Game.Models;
+using Microsoft.EntityFrameworkCore;
 
-// namespace Game.Data
-// {
-//     public class DataContext : DbContext
-//     {
-//         public DataContext(DbContextOptions<DataContext> options) : base(options) { }
-//         public DbSet<Character> Characters { get; set; }
-//     }
-// }
+namespace Game.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseMySQL("server=localhost;database=library;user=user;password=password");
+        }
+        public DbSet<Player> Players { get; set; }
+        public DbSet<Building> Buildings { get; set; }
+    }
+}
