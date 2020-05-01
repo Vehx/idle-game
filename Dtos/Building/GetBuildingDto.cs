9533b87ad444096a1029dd4780e763b9fbd91e6e
vehx <delta222@gmail.com>
2020-05-01 15:15:18 +0200

    added building dtos and game service files

---
+++ Dtos/Building/GetBuildingDto.cs
@@ -0,0 +1,17 @@
+
+using Game.Models;
+
+namespace Game.Dtos
+{
+    public class GetBuildingDto
+    {
+        public int Id { get; set;}
+        public string Name { get; set;} = "Farm";
+        public int Owned { get; set;} = 0;
+        public int Cost { get; set;} = 100;
+        public int IncomeIncrease { get; set;} = 1;
+        public double CostIncrease { get; set;} = 1.2;
+        public IdleUpgrades Upgrade { get; set;} = IdleUpgrades.DoubleIncome;
+    }
+
+}
--- Services/GameService/IGameService.cs
+++ Services/GameService/IGameService.cs
@@ -1,16 +1,16 @@
-// using System.Collections.Generic;
-// using System.Threading.Tasks;
-// using Game.Models;
-// using Game.Dtos.Building;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Game.Models;
+// using Game.Dtos;
 
-// namespace Game.Services
-// {
-//     public interface IGameService
-//     {
-//         Task<ServiceResponse<List<GetBuildingDto>>> GetAllBuildings();
-//         Task<ServiceResponse<GetBuildingDto>> GetBuildingById(int id);
-//         Task<ServiceResponse<List<GetBuildingDto>>> AddBuilding(AddBuildingDto newBuilding);
-//         Task<ServiceResponse<GetBuildingDto>> UpdateBuilding(UpdateBuildingDto updatedBuilding);
-//         Task<ServiceResponse<List<GetBuildingDto>>> DeleteBuilding(int id);
-//     } 
-// }
+namespace Game.Services
+{
+    public interface IGameService
+    {
+        // Task<ServiceResponse<List<GetBuildingDto>>> GetAllBuildings();
+        // Task<ServiceResponse<GetBuildingDto>> GetBuildingById(int id);
+        // Task<ServiceResponse<List<GetBuildingDto>>> AddBuilding(AddBuildingDto newBuilding);
+        // Task<ServiceResponse<GetBuildingDto>> UpdateBuilding(UpdateBuildingDto updatedBuilding);
+        // Task<ServiceResponse<List<GetBuildingDto>>> DeleteBuilding(int id);
+    } 
+}
