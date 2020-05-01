0c4c11ccc9e2c829a652801f7fb3198cc5c6d83b
vehx <delta222@gmail.com>
2020-05-01 15:57:57 +0200

    added mysql migrations

--- Startup.cs
+++ Startup.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using AutoMapper;
+using Game.Data;
 using Game.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
@@ -39,6 +40,8 @@ namespace idle_game
                                             "http://localhost:5000/player/*");
                     });
             });
+
+            services.AddDbContext<DataContext>();
 
             services.AddControllers();
 
