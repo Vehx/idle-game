94a09db237aaa4bb2f1dd41a29a9e57c828c81ea
vehx <delta222@gmail.com>
2020-05-03 14:53:52 +0200

    changed dto to send only needed information

--- Dtos/Player/UpdatePlayerDto.cs
+++ Dtos/Player/UpdatePlayerDto.cs
@@ -6,10 +6,7 @@ namespace Game.Dtos.Player
     public class UpdatePlayerDto
     {
         public int Id { get; set;}
-        public string Name { get; set;} = "Asha";
-        public int Money { get; set;} = 100;
-        public int Income { get; set;} = 0;
-        public List<Building> Buildings { get; set;} = new List<Building> { new Building() };
+        public int BuildingId { get; set;}
     }
 
 }
